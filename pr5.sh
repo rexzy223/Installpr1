c34b1fd2747931da04b5d37667271e80d2a80b31
rexzy223 <rexzyforex@gmail.com>
2025-12-25 21:25:33 +0800

    Update pr5.sh

--- pr5.sh
+++ pr5.sh
@@ -56,7 +56,7 @@ class NestController extends Controller
         // 🔒 Proteksi: hanya user ID 1 (superadmin) yang bisa akses menu Nest
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '🚫 Akses ditolak! Hanya admin ID 1 yang dapat membuka menu Nest! ©Protect By @Rexzystr.');
+            abort(403, '🚫 Akses ditolak! Hanya admin ID 1 yang dapat membuka menu Nest.');
         }
 
         return $this->view->make('admin.nests.index', [
